a570bc33dd09a575162694997a89d8250ae25338
Allen <plus168109@gmail.com>
2023-12-25 14:46:28 +0800

    :w :w

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Hero } from "../hero";
+import { CommonModule } from "@angular/common";
+import {UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    CommonModule,
+    UpperCasePipe,
+    FormsModule
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+hero : Hero = {
+  id : 1,
+  name : 'Windstorm'
+};
+}
